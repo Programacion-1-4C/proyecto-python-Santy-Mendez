c8f06d9853d9e563b88e24836664ae5f0d2d3f07
Santy <juansantiagosalvaymendez@gmail.com>
2022-08-11 11:15:55 -0300

    quinto commit

--- Programa.py
+++ Programa.py
@@ -1,4 +1,4 @@
-
+from funciones import org_lista
 print("BIENVENIDO AL ALGORITMO PARA GRAFICAS")
 persona = input("Ingrese VENDEDOR si usted es el vendedor de lo contrario escriba CONSUMIDOR\n>>>")
 if persona == "VENDEDOR":
@@ -7,15 +7,18 @@ if persona == "VENDEDOR":
     if usuario == "over" and contraseña == "over145@":
         sesion = "activada"
         while sesion == "activada":
-            print("1. Poner Stock")
-            print("2. Cambiar Stock")
-            print("3. Salir")
+            print("1. Crear lista")
+            print("2. Poner Stock")
+            print("3. Cambiar Stock")
+            print("4. Salir")
             opcion = int(input("Ingrese una opcion\n>>>"))
-            if opcion == "1":
-                pass
+            if opcion == 1:
+                org_lista()
             if opcion == "2":
                 pass
             if opcion == "3":
+                pass
+            if opcion == "4":
                 break
 
 
--- funciones.py
+++ funciones.py
@@ -1,3 +1,31 @@
-def Poner_stock (num):
-    lista_graficas = []
-    n = int(input(""))
+def org_lista():
+    lista_producto = []
+    cantidad = int(input("Ingrese la cantidad de listas que quiere crear\n>>>"))
+    for i in range(1):
+        print("Cantidad:       "*cantidad)
+    for i in range(1):
+        print("Marca:          "*cantidad)
+    for i in range(1):
+        print("Chipset:        "*cantidad)
+    for i in range(1):
+        print("Modelo:         "*cantidad)
+    for i in range(1):
+        print("Memoria:        "*cantidad)
+    for i in range(1):
+        print("Disipador:      "*cantidad)
+    for i in range(1):
+        print("Puerto:         "*cantidad)
+    for i in range(1):
+        print("RGB:            "*cantidad)
+    for i in range(1):
+        print("Precio:         "*cantidad)
+
+
+
+
+
+
+
+
+
+
