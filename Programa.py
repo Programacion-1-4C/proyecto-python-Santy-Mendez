b36a2fb1314684b4ba8f9e6e9f7ef1453ab5d069
Santy <juansantiagosalvaymendez@gmail.com>
2022-08-17 13:32:09 -0300

    septimo commit

--- Programa.py
+++ Programa.py
@@ -1,5 +1,5 @@
-from funciones import org_lista
-todas_listas= { }
+from funciones import org_lista, mostrar_lista
+todas_listas =[]
 print("BIENVENIDO AL ALGORITMO PARA GRAFICAS")
 persona = input("Ingrese VENDEDOR si usted es el vendedor de lo contrario escriba CONSUMIDOR\n>>>")
 persona_1 = persona.upper()
@@ -10,17 +10,31 @@ if persona_1== "VENDEDOR":
         sesion = "activada"
         while sesion == "activada":
             print("1. Poner Stock")
-            print("2. Cambiar Stock")
-            print("3. Salir")
+            print("2. Ver Listas")
+            print("3. Cambiar Informacion")
+            print("4. Salir")
             opcion = int(input("Ingrese una opcion\n>>>"))
             if opcion == 1:
-                pass
+                org_lista(todas_listas)
             if opcion == 2:
-                pass
+                mostrar_lista(todas_listas)
             if opcion == 3:
                 pass
             if opcion == 4:
+                print("Salir")
                 break
-
-
+    if usuario != "over" or contraseña != "over145@":
+        sesion = "no activada"
+        while sesion == "no activada":
+            print("1 . Comprar Grafica")
+            print("2.  Consultar Precio")
+            print("3 .  Salir")
+            opcions = int(input("Ingrese una opcion\n>>>"))
+            if opcions == 1:
+                pass
+            if opcions == 2:
+                pass
+            if opcions == 3:
+                print("Vuelva Pronto!")
+                break
 
--- funciones.py
+++ funciones.py
@@ -1,44 +1,45 @@
-def org_lista():
-    lista_producto = []
+def org_lista(todas_listas):
     nombre = input("Ingrese el nombre de la lista\n>>>")
     cantidad = int(input("Ingrese la cantidad de graficas de este tipo que tiene\n>>>"))
     marca = input("Ingrese la marca que vende el producto\n>>>")
-    chipset= input("Ingrese el fabricante\n>>>")
-    modelo= input("Ingrese el modelo\n>>>")
-    memoria= int(input("Ingrese la cantidad de memoria\n>>>"))
-    refrigeracion= input("Especifique el tipo de refrigeracion y en caso de tener disipadores especifique cuantos\n>>>")
-    puertos=int(input("Ingrese la cantidad de puertos\n>>>"))
-    rgb=input("Especifique si cuenta con retroiluminacion\n>>>")
-    precio= int(input("Ingrese el precio del producto\n>>>"))
-    for i in range(1):
-        print("Nombre De Lista:", nombre)
-    for i in range(1):
-        print("Cantidad:", cantidad)
-    for i in range(1):
-        print("Marca:", marca)
-    for i in range(1):
-        print("Chipset:", chipset)
-    for i in range(1):
-        print("Modelo:", modelo)
-    for i in range(1):
-        print("Memoria:", memoria)
-    for i in range(1):
-        print("Refrigeracion:", refrigeracion)
-    for i in range(1):
-        print("Puerto:", puertos)
-    for i in range(1):
-        print("RGB:",   rgb, "GB")
-    for i in range(1):
-        print("Precio:",  precio)
-    print("--------------------------------")
+    chipset = input("Ingrese el fabricante\n>>>")
+    modelo = input("Ingrese el modelo\n>>>")
+    memoria = int(input("Ingrese la cantidad de memoria\n>>>"))
+    refrigeracion = input(
+        "Especifique el tipo de refrigeracion y en caso de tener disipadores especifique cuantos\n>>>")
+    puertos = int(input("Ingrese la cantidad de puertos\n>>>"))
+    rgb = input("Especifique si cuenta con retroiluminacion\n>>>")
+    precio = int(input("Ingrese el precio del producto\n>>>"))
+    todas_listas.append([
+        ("Nombre de la Lista:", nombre),
+        ("Cantidad de graficas:", cantidad),
+        ("Marca que vende el producto:", marca),
+        ("Cantidad de graficas:", cantidad),
+        ("Marca que vende el producto:", marca),
+        ("Fabircante del Chipset:", chipset),
+        ("Modelo de la grafica:", modelo),
+        ("Cantidad de Mmeoria:", memoria),
+        ("Info de refrigeracion:", refrigeracion),
+        ("Cantidad de Puertos:", puertos),
+        ("Cuenta con RGB:", rgb),
+        ("Precio del producto:", precio),
+    ])
 
 
+def mostrar_lista(lista):
+    for sub_lista in lista:
+        for elemento in sub_lista:
+            print(elemento[0], " ", elemento[1])
+        print("-"*50)
 
-
-
-
-
-
+def cambiar_datos(todas_listas):
+    nombre = input("Ingrese el nombre de la lista")
+    for sub_lista in todas_listas:
+        if sub_lista[0][1] == nombre:
+            elemento_1= input("Ingrese que parte de la lista quiere cambiar ")
+            for j in sub_lista:
+                if j[0] == elemento_1:
+                    j[1] = input("Ingrese el nuevo dato")
 
 
 
